34e034635fd9e97fcfe4607457eb1551900b1d10
dsGitHubRepository <dsGitHubRepository@users.noreply.github.com>
2017-04-05 18:48:15 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 python ./src/process_logQ1.py ./log_input/log.txt ./log_output/hosts.txt
+
+python ./src/process_logQ1.py ./log_input/log.txt ./log_output/hours.txt
+
+python ./src/process_logQ1.py ./log_input/log.txt ./log_output/resources.txt
+
+python ./src/process_logQ1.py ./log_input/log.txt ./log_output/blocked.txt
