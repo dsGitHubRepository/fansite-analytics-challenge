3b72dd38cda30f54d8a11553e084f7f1bbce1edd
dsGitHubRepository <dsGitHubRepository@users.noreply.github.com>
2020-04-30 17:21:14 -0400

    Update process_logQ4.py

--- src/process_logQ4.py
+++ src/process_logQ4.py
@@ -54,51 +54,14 @@ FMT= '%d/%b/%Y:%H:%M:%S'  # timestamp format for the input data
 # Since we are searching for multiple consecutive '401' failed attempt;
 # indices for '401' be checked for consecutive order
  
-for ncomp, h in groupby(enumerate(httpIndex401), lambda (i, x): i-x):
-    Indexhttpgroup=map(itemgetter(1), h)        # consecutive indexList[] 
-    SizeIndexgroup=np.size(Indexhttpgroup)      
-    CFS=4   # CFS = consecutive failed attempts: 3 times or 4 times etc
-    if SizeIndexgroup==CFS:
-        ipRecall=[]
-        for index in Indexhttpgroup:
-            ipRecall.append(ipList1[index])  
-            count=Counter(ipRecall)  
-            ipSize=np.size(ipRecall)
-            CountMostcommon=count.most_common() 
-            ipwithIndex=CountMostcommon[0] 
-            FofR=ipwithIndex[1]    # to make sure taht successive '401' from the same ip; 
-            ipcheck1=ipwithIndex[0]
-            if ipSize==FofR:   # test for ip uniqueness
-                if FofR==CFS:
-                    t1index=Indexhttpgroup[0] 
-                    time1=tstmpList1[t1index]   # timestamp for 1st failed attemp
-                    t2index=Indexhttpgroup[CFS-1]   
-                    time2=tstmpList1[t2index]   # timestamp for Nth=(NOSFA)-th failed attemp
-                    timeDiff=datetime.datetime.strptime(time2, FMT) - datetime.datetime.strptime(time1,FMT)
-                    timeDiffSec=timeDiff.seconds
-                    #print "timeDiffsec", timeDiffSec
-                    timeStep=5     # Nth failed attempt over timeStep (20 sec or 5 sec etc)
-                    nt=0
-                    if timeDiffSec==timeStep:   # timedifference for successive failed attempt 
-                        immediateIndex=t2index+1
-                        npSizehttpreplyList1=np.size(httpreplyList1)
-                        for icode in range(immediateIndex,npSizehttpreplyList1):
-                            httpi=httpreplyList1[icode]
-                            ipcheck2=ipList1[icode]
-                            if (httpi=='304' and ipcheck1==ipcheck2): # to make sure that it is from the same ip
-                                index304=icode
-                                time1_304=tstmpList1[index304]
-                                timeDelta=datetime.datetime.strptime(time1_304, FMT) - datetime.datetime.strptime(time2,FMT)
-                                timeDeltaseconds=timeDelta.seconds
-                                #print timeDeltaseconds
-                                timestepBlock=300   # 5 minutes etc
-                                if timeDeltaseconds<=timestepBlock:
-                                    print  ipList1[icode],"--",tstmpList1[icode],";",httpreplyList1[icode]
-                                    datablockedout.write('%s %s %s %s %s %s\n' % (ipList1[icode],"--",\
-                                    tstmpList1[icode],";",rsrcList[icode], httpreplyList1[icode]))
-                                    
-                                    
-                                
+# 
+
+
+
+
+
+
+
 datablockedout.close()                               
                             
                             
